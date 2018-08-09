8cb829fff8cd686f96d38aad727df873af6f379e
kazinur <kazinur@mybihs.org>
2018-08-09 20:27:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,59 @@
-# dog.rb
+class Dog 
+end
+#we are using end because we need to end the class. 
+fido = Dog.new
+snoopy = Dog.new 
+lassie = Dog.new 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # def initialize (name, name1, name2)
+  #     @name= name 
+  #     @name1=name1
+  #     @name2= name2
+  # end 
+     
+  #   def name 
+  #     @name
+  #   end 
+     
+  #   def name1
+  #     @name1
+  #   end 
+     
+  #   def name2 
+  #     @name2
+  #   end 
+
+   
+  # dogs_name= Dog.new("fido","snoopy", "lassie")
+  #   puts dogs_name.name
+  #   puts dogs_name.name1
+  #   puts dogs_name.name2 
+     
+
